bc678d468b0dc02a242aab44d0289c78c8d5b148
AAA-Dev <dazehr22@gmail.com>
2017-10-04 14:50:04 -0400

    Converted the abstract class  [HeroClixMapFactory.cs] to an interface and created a concrete class which uses that interface to create HeroClixMaps.

--- HeroClix/Map/HeroClixMapFactory.cs
+++ HeroClix/Map/HeroClixMapFactory.cs
@@ -6,18 +6,125 @@ using System.Threading.Tasks;
 
 namespace HeroClix.Map
 {
-    public abstract class HeroClixMapFactory
+    public class HeroClixMapFactory : IHeroClixMapFactory
     {
-        public abstract IHeroClixMap CreateIndoorMap(string setName, string mapName);
-        public abstract IHeroClixMap CreateIndoorMap(string setName, string mapName, int rows, int columns);
-        public abstract IHeroClixMap CreateIndoorMap(string setName, string mapName, Tile[,] tiles);
+        private IntellectualProperty intellectualProperty;
 
-        public abstract IHeroClixMap CreateOutdoorMap(string setName, string mapName);
-        public abstract IHeroClixMap CreateOutdoorMap(string setName, string mapName, int rows, int columns);
-        public abstract IHeroClixMap CreateOutdoorMap(string setName, string mapName, Tile[,] tiles);
+        /// <summary>
+        /// Creates a new HeroClixMapFactory.
+        /// </summary>
+        /// <param name="IP">The IntellectualProperty of the HeroClixMaps that will be made by this factory.</param>
+        public HeroClixMapFactory(IntellectualProperty IP)
+        {
+            this.intellectualProperty = IP;
+        }
 
-        public abstract IHeroClixMap CreateIndoorOutdoorMap(string setName, string mapName);
-        public abstract IHeroClixMap CreateIndoorOutdoorMap(string setName, string mapName, int rows, int columns);
-        public abstract IHeroClixMap CreateIndoorOutdoorMap(string setName, string mapName, Tile[,] tiles);
+        /// <summary>
+        /// Creates an indoor HeroClix map of standard size filled with clear Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <returns>An indoor HeroClix map.</returns>
+        public IHeroClixMap CreateIndoorMap(string setName, string mapName)
+        {
+            return new IndoorMap(this.intellectualProperty, setName, mapName);
+        }
+
+        /// <summary>
+        /// Creates an indoor HeroClix map of the specified dimensions filled with clear Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <param name="rows">The number of rows the map will have.</param>
+        /// <param name="columns">The number of columns the map will have.</param>
+        /// <returns>An indoor HeroClix map.</returns>
+        public IHeroClixMap CreateIndoorMap(string setName, string mapName, int rows, int columns)
+        {
+            return new IndoorMap(this.intellectualProperty, setName, mapName, rows, columns);
+        }
+
+        /// <summary>
+        /// Creates an indoor HeroClix map using the specified 2D array of Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <param name="tiles">The 2D array of Tiles which make up the map.</param>
+        /// <returns>An indoor HeroClix map.</returns>
+        public IHeroClixMap CreateIndoorMap(string setName, string mapName, Tile[,] tiles)
+        {
+            return new IndoorMap(this.intellectualProperty, setName, mapName, tiles);
+        }
+
+        /// <summary>
+        /// Creates an outdoor HeroClix map of standard size filled with clear Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <returns>An outdoor HeroClix map.</returns>
+        public IHeroClixMap CreateOutdoorMap(string setName, string mapName)
+        {
+            return new OutdoorMap(this.intellectualProperty, setName, mapName);
+        }
+
+        /// <summary>
+        /// Creates an outdoor HeroClix map of the specified dimensions filled with clear Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <param name="rows">The number of rows the map will have.</param>
+        /// <param name="columns">The number of columns the map will have.</param>
+        /// <returns>An outdoor HeroClix map.</returns>
+        public IHeroClixMap CreateOutdoorMap(string setName, string mapName, int rows, int columns)
+        {
+            return new OutdoorMap(this.intellectualProperty, setName, mapName, rows, columns);
+        }
+
+        /// <summary>
+        /// Creates an outdoor HeroClix map using the specified 2D array of Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <param name="tiles">The 2D array of Tiles which make up the map.</param>
+        /// <returns>An outdoor HeroClix map.</returns>
+        public IHeroClixMap CreateOutdoorMap(string setName, string mapName, Tile[,] tiles)
+        {
+            return new OutdoorMap(this.intellectualProperty, setName, mapName, tiles);
+        }
+
+        /// <summary>
+        /// Creates an indoor/outdoor HeroClix map of standard size filled with clear Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <returns>An indoor/outdoor HeroClix map.</returns>
+        public IHeroClixMap CreateIndoorOutdoorMap(string setName, string mapName)
+        {
+            return new IndoorOutdoorMap(this.intellectualProperty, setName, mapName);
+        }
+
+        /// <summary>
+        /// Creates an indoor/outdoor HeroClix map of the specified dimensions filled with clear Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <param name="rows">The number of rows the map will have.</param>
+        /// <param name="columns">The number of columns the map will have.</param>
+        /// <returns>An indoor/outdoor HeroClix map.</returns>
+        public IHeroClixMap CreateIndoorOutdoorMap(string setName, string mapName, int rows, int columns)
+        {
+            return new IndoorOutdoorMap(this.intellectualProperty, setName, mapName, rows, columns);
+        }
+
+        /// <summary>
+        /// Creates an indoor/outdoor HeroClix map using the specified 2D array of Tiles.
+        /// </summary>
+        /// <param name="setName">The name of the set that the HeroClix map being created belongs to.</param>
+        /// <param name="mapName">The name of the HeroClix map being created.</param>
+        /// <param name="tiles">The 2D array of Tiles which make up the map.</param>
+        /// <returns>An indoor/outdoor HeroClix map.</returns>
+        public IHeroClixMap CreateIndoorOutdoorMap(string setName, string mapName, Tile[,] tiles)
+        {
+            return new IndoorOutdoorMap(this.intellectualProperty, setName, mapName, tiles);
+        }
     }
 }
