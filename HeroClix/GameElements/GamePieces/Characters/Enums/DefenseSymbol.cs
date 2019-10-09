467a682259bd2100701f080a7de20530c09f8a5f
David Zehr <dazehr22@gmail.com>
2019-10-09 10:40:06 -0400

    Updating Symbol Enums to reflect their actual names. Added 'Autonomous' to Attack Symbols and 'Vehicle' to Defense Symbols.

--- HeroClix/GameElements/GamePieces/Characters/Enums/DefenseSymbol.cs
+++ HeroClix/GameElements/GamePieces/Characters/Enums/DefenseSymbol.cs
@@ -6,6 +6,7 @@
     public enum DefenseSymbol
     {
         Standard,
-        Stripe
+        Indomitable,
+        Vehicle
     }
 }
--- HeroClix/GameElements/GamePieces/Characters/Enums/SpeedSymbol.cs
+++ HeroClix/GameElements/GamePieces/Characters/Enums/SpeedSymbol.cs
@@ -6,7 +6,7 @@
     public enum SpeedSymbol
     {
         Standard,
-        Wing,
-        Dolphin
+        Flight,
+        Swim
     }
 }
